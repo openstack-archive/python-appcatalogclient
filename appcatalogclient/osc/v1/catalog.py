f00d64586fe76853bb4fec5ceea632566c3a0b4b
Anh Tran <anhtt@vn.fujitsu.com>
2017-02-20 09:31:04 +0700

    Remove unused logging import
    Change-Id: Ia0c64437b713d968b858bd72f6b7007b9c277422

--- appcatalogclient/osc/v1/catalog.py
+++ appcatalogclient/osc/v1/catalog.py
@@ -11,12 +11,8 @@
 #   License for the specific language governing permissions and limitations
 #   under the License.
 
-import logging
-
 from cliff import lister
 from cliff import show
-
-LOG = logging.getLogger(__name__)
 
 
 class ListApps(lister.Lister):
